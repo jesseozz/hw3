2859a950b752a5ca24e1b61cc5072beea8273781
jesse ozdamar <jesse.ozdamar@kellogg.northwestern.edu>
2024-02-15 20:46:11 +0000

    not quite

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,12 +1,12 @@
 class EntriesController < ApplicationController
   skip_before_action :verify_authenticity_token 
   def index
-    @entry = Entry.all
+    @entries = Entry.all
     # render :template => "tacos/index"
   end
 
   def show
-    # find a Place
+    # find an entry
     @entry = Entry.find_by({ "id" => params["id"] })
     # find Entries for the Company
     # render companies/show view with details about Company
@@ -20,9 +20,11 @@ class EntriesController < ApplicationController
     # start with a new Place
     @entry = Entry.new
 
-    # assign user-entered form data to Place's columns
+    # assign user-entered form data to Entry's columns
+    @entry["place_id"] = Place.find_by({ "id" => params["place"] })
     @entry["title"] = params["title"]
-    
+    @entry["posted_on"] = params["posted_on"]
+    @entry["description"] = params["description"]
 
     # save Place row
     @entry.save
@@ -30,7 +32,5 @@ class EntriesController < ApplicationController
     # redirect user
     redirect_to "/entries"
   end
-
-end
 
 end
